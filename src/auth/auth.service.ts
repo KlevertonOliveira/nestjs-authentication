a925c3426b148bbbfca7c9c4d80e6d23f8a77eb0
KlevertonOliveira <kleverton.oliveira.al@gmail.com>
2023-03-02 16:15:42 -0300

    implement login method in AuthService

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async validateUser(username: string, passwd: string) {
     const user = await this.usersService.findOne(username);
@@ -12,5 +16,12 @@ export class AuthService {
 
     const { password, ...result } = user;
     return result;
+  }
+
+  async login(user: any) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
   }
 }
